dde6a1f98b5d4d1aa8487826742267235c803774
Your Name <you@example.com>
2023-07-17 11:51:24 -0400

    aider: Added the `OpenAIFunctions` agent with the `perform_action` method.
    # Aider chat conversation:
    
    USER: continue
    
    ASSISTANT: Alright, let's proceed with the next agent, the `OpenAIFunctions` agent. This agent is designed to work with OpenAI models that have been fine-tuned to detect when a function should be called and respond with the inputs that should be passed to the function.
    
    Here's how we can define this agent:
    
    ```python
    class OpenAIFunctions:
        def __init__(self, tools):
            self.tools = tools
    
        def perform_action(self, action_input):
            # action_input is a structured input
            # Use the tools based on the structured input
            pass
    ```
    
    We'll need to define the `perform_action` method based on the specific tools and the structure of the action input.
    
    Let's add this agent to the `agents` directory.
    
    ```python
    agents/openai_functions.py
    <<<<<<< ORIGINAL
    
    =======
    class OpenAIFunctions:
        def __init__(self, tools):
            self.tools = tools
    
        def perform_action(self, action_input):
            # action_input is a structured input
            # Use the tools based on the structured input
            pass
    >>>>>>> UPDATED
    ```
    
    Please confirm if this is the correct approach for the `OpenAIFunctions` agent. Once confirmed, we can proceed with the other agents.

---
+++ agents/openai_functions.py
@@ -0,0 +1,8 @@
+class OpenAIFunctions:
+    def __init__(self, tools):
+        self.tools = tools
+
+    def perform_action(self, action_input):
+        # action_input is a structured input
+        # Use the tools based on the structured input
+        pass
