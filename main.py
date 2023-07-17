3cceda80db1c58e2e8915f7e1c9b39da3260a719
Your Name <you@example.com>
2023-07-17 12:45:15 -0400

    aider: Set llm_gpt35turbo as the default language model and prompt the user for input when the application starts.
    # Aider chat conversation:
    
    USER: how do I interact with app?
    
    ASSISTANT: The application is a command-line interface (CLI) application. Here's how you can interact with it:
    
    1. Run the application: You can run the application by executing the `main.py` script in your terminal. Make sure you are in the correct directory where the `main.py` file is located. Use the command `python main.py` to run the application.
    
    2. Use the commands: Once the application is running, you will see a prompt `langchain> `. You can type commands at this prompt. Here are the available commands:
    
       - `greet`: This command will print a greeting message.
       - `quit`: This command will exit the application.
       - `select_model <model_name>`: This command allows you to select the language model. Replace `<model_name>` with one of the following: `gpt-4`, `gpt-3.5-turbo-16k`, or `gpt-3.5-turbo`.
       - `chat <message>`: This command allows you to chat with the selected language model. Replace `<message>` with your message.
    
    3. Exit the application: You can exit the application by typing the `quit` command at the prompt.
    
    Please note that you need to have the OpenAI key set in your environment variables to use this application. You can set this in the `.env` file.
    
    USER: ok. can we make the behavior to be ready for user prompt and default the llm to llm_gpt35turbo?
    
    ASSISTANT: Sure, we can make the `llm_gpt35turbo` as the default language model and prompt the user for input when the application starts. Here are the changes we need to make in the `main.py` file:
    
    1. Set `llm_gpt35turbo` as the default language model in the `__init__` method of the `LangchainCLI` class.
    2. Override the `preloop` method of the `cmd.Cmd` class to print a welcome message and prompt the user for input when the application starts.
    
    Here are the proposed changes:
    
    ```python
    main.py
    <<<<<<< ORIGINAL
    class LangchainCLI(cmd.Cmd):
        prompt = 'langchain> '
    
        def __init__(self):
            super().__init__()
            self.llm_gpt4 = LLM("gpt-4")
            self.llm_gpt35turbo16k = LLM("gpt-3.5-turbo-16k")
            self.llm_gpt35turbo = LLM("gpt-3.5-turbo")
            self.current_llm = self.llm_gpt35turbo
    =======
    class LangchainCLI(cmd.Cmd):
        prompt = 'langchain> '
    
        def __init__(self):
            super().__init__()
            self.llm_gpt4 = LLM("gpt-4")
            self.llm_gpt35turbo16k = LLM("gpt-3.5-turbo-16k")
            self.llm_gpt35turbo = LLM("gpt-3.5-turbo")
            self.current_llm = self.llm_gpt35turbo
            print("Hello, welcome to Langchain!")
            print("The default language model is gpt-3.5-turbo.")
            print("You can change the language model using the 'select_model' command.")
            print("You can start chatting with the language model using the 'chat' command.")
            print("Type 'help' for more information on commands.")
    
        def preloop(self):
            self.do_greet(None)
    >>>>>>> UPDATED
    ```
    
    Please confirm if you agree with these changes.

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ class LangchainCLI(cmd.Cmd):
         self.llm_gpt35turbo16k = LLM("gpt-3.5-turbo-16k")
         self.llm_gpt35turbo = LLM("gpt-3.5-turbo")
         self.current_llm = self.llm_gpt35turbo
+        print("Hello, welcome to Langchain!")
+        print("The default language model is gpt-3.5-turbo.")
+        print("You can change the language model using the 'select_model' command.")
+        print("You can start chatting with the language model using the 'chat' command.")
+        print("Type 'help' for more information on commands.")
+
+    def preloop(self):
+        self.do_greet(None)
 
     def do_greet(self, line):
         print("Hello, welcome to Langchain!")
