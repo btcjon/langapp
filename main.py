0d2f8f7b833489cccc15b48ee179f03f6cb951da
Your Name <you@example.com>
2023-07-17 12:34:11 -0400

    aider: Added support for selecting different language models in LangchainCLI.
    # Aider chat conversation:
    
    USER: main.py
    
    ASSISTANT: The `main.py` file currently contains a `LangchainCLI` class with `do_greet` and `do_quit` methods. If you want to make changes to this file, please provide more specific details about what these changes should be.
    
    For example, do you want to add more methods to the `LangchainCLI` class? Or do you want to modify the existing `do_greet` and `do_quit` methods? Please provide more details so I can provide the appropriate code changes.
    
    USER: in our langchain framework, how do I specify gpt-4,gpt-3.5-turbo-16k,gpt-3.5-turbo as seperate LLMs and how does it choose which one to use?
    
    ASSISTANT: To specify different language models like GPT-4, GPT-3.5-turbo-16k, and GPT-3.5-turbo as separate LLMs (Language Learning Models) in your Langchain framework, you would need to create separate instances of the LLM class (or whatever class you're using to interact with the OpenAI API) for each model.
    
    Each instance would be initialized with the model name as a parameter, and you would use the appropriate instance whenever you want to use a specific model.
    
    However, the choice of which model to use at any given time would depend on the specific requirements of your application. You could implement a strategy pattern to select the appropriate model based on certain conditions, or you could allow the user to specify the model as an input parameter.
    
    Please note that as of now, GPT-4 and GPT-3.5-turbo-16k are not available models. You can check the available models in the OpenAI documentation.
    
    If you provide more details about how you want to implement this, I can provide more specific code changes.
    
    USER: I can verify that these models are available. let's implement this
    
    ASSISTANT: To implement this, we need to modify the `main.py` file to create separate instances of the LLM class for each model and add a method to the `LangchainCLI` class to allow the user to select a model.
    
    Here are the proposed changes:
    
    ```python
    main.py
    <<<<<<< ORIGINAL
    class LangchainCLI(cmd.Cmd):
        prompt = 'langchain> '
    
        def do_greet(self, line):
            print("Hello, welcome to Langchain!")
    
        def do_quit(self, line):
            return True
    =======
    class LangchainCLI(cmd.Cmd):
        prompt = 'langchain> '
    
        def __init__(self):
            super().__init__()
            self.llm_gpt4 = LLM("gpt-4")
            self.llm_gpt35turbo16k = LLM("gpt-3.5-turbo-16k")
            self.llm_gpt35turbo = LLM("gpt-3.5-turbo")
            self.current_llm = self.llm_gpt4
    
        def do_greet(self, line):
            print("Hello, welcome to Langchain!")
    
        def do_quit(self, line):
            return True
    
        def do_select_model(self, line):
            if line == "gpt-4":
                self.current_llm = self.llm_gpt4
            elif line == "gpt-3.5-turbo-16k":
                self.current_llm = self.llm_gpt35turbo16k
            elif line == "gpt-3.5-turbo":
                self.current_llm = self.llm_gpt35turbo
            else:
                print("Invalid model name")
    >>>>>>> UPDATED
    ```
    
    In this updated version, the `LangchainCLI` class has three new attributes: `llm_gpt4`, `llm_gpt35turbo16k`, and `llm_gpt35turbo`, which are instances of the LLM class initialized with the names of the different models. It also has a `current_llm` attribute, which is the LLM instance that will be used for the current session. By default, it's set to `llm_gpt4`.
    
    The `do_select_model` method allows the user to select a model by typing "select_model gpt-4", "select_model gpt-3.5-turbo-16k", or "select_model gpt-3.5-turbo" in the CLI.
    
    Please replace `LLM` with the name of the class you're using to interact with the OpenAI API, and add the necessary import statement at the beginning of the file. Also, handle the exceptions as necessary.

--- main.py
+++ main.py
@@ -12,11 +12,28 @@ openai.api_key = os.getenv("OPENAI_KEY")
 class LangchainCLI(cmd.Cmd):
     prompt = 'langchain> '
 
+    def __init__(self):
+        super().__init__()
+        self.llm_gpt4 = LLM("gpt-4")
+        self.llm_gpt35turbo16k = LLM("gpt-3.5-turbo-16k")
+        self.llm_gpt35turbo = LLM("gpt-3.5-turbo")
+        self.current_llm = self.llm_gpt4
+
     def do_greet(self, line):
         print("Hello, welcome to Langchain!")
 
     def do_quit(self, line):
         return True
+
+    def do_select_model(self, line):
+        if line == "gpt-4":
+            self.current_llm = self.llm_gpt4
+        elif line == "gpt-3.5-turbo-16k":
+            self.current_llm = self.llm_gpt35turbo16k
+        elif line == "gpt-3.5-turbo":
+            self.current_llm = self.llm_gpt35turbo
+        else:
+            print("Invalid model name")
 
 if __name__ == '__main__':
     LangchainCLI().cmdloop()
