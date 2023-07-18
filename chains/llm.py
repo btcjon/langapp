9af3f8e90e13bc6643688da10dfdd2be5d15b50f
Your Name <you@example.com>
2023-07-17 20:48:34 -0400

    aider: Add 'memory' attribute to the 'LLM' class and initialize it in the '__init__' method.
    # Aider chat conversation:
    
    USER: Hello, welcome to Langchain!
    The default language model is gpt-3.5-turbo.
    You can change the language model using the 'select_model' command.
    You can start chatting with the language model using the 'chat' command.
    Type 'help' for more information on commands.
    Traceback (most recent call last):
      File "/Users/jonbennett/Dropbox/Coding/langapp/main.py", line 65, in <module>
        LangchainCLI().cmdloop()
      File "/Users/jonbennett/Dropbox/Coding/langapp/main.py", line 32, in __init__
        self.do_chat('')
      File "/Users/jonbennett/Dropbox/Coding/langapp/main.py", line 58, in do_chat
        response = self.current_llm.chat([{"role": "system", "content": "You are a helpful assistant."}])
      File "/Users/jonbennett/Dropbox/Coding/langapp/chains/llm.py", line 11, in chat
        self.memory.store(messages[0]["role"], messages[0]["content"])
    AttributeError: 'LLM' object has no attribute 'memory'
    
    ASSISTANT: The error message indicates that the 'LLM' object has no attribute 'memory'. This is because the 'memory' attribute is not defined in the 'LLM' class.
    
    To fix this, we need to add a 'memory' attribute to the 'LLM' class and initialize it in the '__init__' method.
    
    Here are the changes needed:
    
    ```python
    chains/llm.py
    <<<<<<< ORIGINAL
    class LLM:
        def __init__(self, model_name):
            self.model_name = model_name
    =======
    import memory.short_term.memory as mem
    
    class LLM:
        def __init__(self, model_name):
            self.model_name = model_name
            self.memory = mem.Memory()
    >>>>>>> UPDATED
    ```
    
    This change will create a new instance of the 'Memory' class for each 'LLM' object, which should resolve the error.

--- chains/llm.py
+++ chains/llm.py
@@ -1,8 +1,11 @@
 import openai
 
+import memory.short_term.memory as mem
+
 class LLM:
     def __init__(self, model_name):
         self.model_name = model_name
+        self.memory = mem.Memory()
 
     def execute(self):
         pass
